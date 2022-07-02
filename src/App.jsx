bbba42b1aa8aece852228560d8b6bda5a77ab3ed
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-07-03 00:34:12 +0530

    Done with Product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Home from './pages/Home';
+import Product from './pages/Product';
 import ProductList from './pages/ProductList';
 
 const App = () => {
   return (
     <div>
     {/* <Home /> */}
-    <ProductList />
+    <Product />
     </div>
   )
 }
