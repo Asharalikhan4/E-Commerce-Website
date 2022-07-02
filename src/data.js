1c1e489c75744bb652d7606a269e5095be227d03
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-07-02 11:46:59 +0530

    Category item added

--- src/data.js
+++ src/data.js
@@ -4,38 +4,73 @@ export const sliderItems = [
     img: "https://lp2.hm.com/hmgoepprod?set=source[/ed/ac/edac79333b5b8a044a5ff92242a2e5bfef380c13.jpg],origin[dam],category[men_tshirtstanks_printed],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
     title: "SUMMER SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FROM NEW ARRIVALS",
-    bg: "f5fafd"
+    bg: "f5fafd",
 },
 {
     id: 2,
     img: "https://lp2.hm.com/hmgoepprod?set=source[/ed/ac/edac79333b5b8a044a5ff92242a2e5bfef380c13.jpg],origin[dam],category[men_tshirtstanks_printed],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
     title: "AUTUMN SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FROM NEW ARRIVALS",
-    bg: "fcf1ed"
+    bg: "fcf1ed",
 },
 {
     id: 3,
     img: "https://lp2.hm.com/hmgoepprod?set=source[/ed/ac/edac79333b5b8a044a5ff92242a2e5bfef380c13.jpg],origin[dam],category[men_tshirtstanks_printed],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
     title: "STREET STYLE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FROM NEW ARRIVALS",
-    bg: "fbf0f4"
+    bg: "fbf0f4",
 }
 ]
 
 export const categories = [
         {
             id: 1,
-            img: "https://lp2.hm.com/hmgoepprod?set=source[/ed/ac/edac79333b5b8a044a5ff92242a2e5bfef380c13.jpg],origin[dam],category[men_tshirtstanks_printed],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
-            title: "STREET STYLE"
+            img: "https://lp2.hm.com/hmgoepprod?set=source[/e5/ff/e5ff221890f640a53d4240a6801d2a1283c36fcf.jpg],origin[dam],category[men_tshirtstanks_multipacks],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+            title: "STREET STYLE",
         },
         {
             id: 2,
-            img: "https://lp2.hm.com/hmgoepprod?set=source[/ed/ac/edac79333b5b8a044a5ff92242a2e5bfef380c13.jpg],origin[dam],category[men_tshirtstanks_printed],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
-            title: "STREET STYLE"
+            img: "https://lp2.hm.com/hmgoepprod?set=source[/e5/ff/e5ff221890f640a53d4240a6801d2a1283c36fcf.jpg],origin[dam],category[men_tshirtstanks_multipacks],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+            title: "STREET STYLE",
         },
         {
             id: 3,
-            img: "https://lp2.hm.com/hmgoepprod?set=source[/ed/ac/edac79333b5b8a044a5ff92242a2e5bfef380c13.jpg],origin[dam],category[men_tshirtstanks_printed],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
-            title: "STREET STYLE"
+            img: "https://lp2.hm.com/hmgoepprod?set=source[/e5/ff/e5ff221890f640a53d4240a6801d2a1283c36fcf.jpg],origin[dam],category[men_tshirtstanks_multipacks],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+            title: "STREET STYLE",
         }
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://lp2.hm.com/hmgoepprod?set=source[/e5/ff/e5ff221890f640a53d4240a6801d2a1283c36fcf.jpg],origin[dam],category[men_tshirtstanks_multipacks],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",     
+    },
+    {
+        id: 2,
+        img: "https://lp2.hm.com/hmgoepprod?set=source[/e5/ff/e5ff221890f640a53d4240a6801d2a1283c36fcf.jpg],origin[dam],category[men_tshirtstanks_multipacks],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",     
+    },
+    {
+        id: 3,
+        img: "https://lp2.hm.com/hmgoepprod?set=source[/e5/ff/e5ff221890f640a53d4240a6801d2a1283c36fcf.jpg],origin[dam],category[men_tshirtstanks_multipacks],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",     
+    },
+    {
+        id: 4,
+        img: "https://lp2.hm.com/hmgoepprod?set=source[/e5/ff/e5ff221890f640a53d4240a6801d2a1283c36fcf.jpg],origin[dam],category[men_tshirtstanks_multipacks],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",     
+    },
+    {
+        id: 5,
+        img: "https://lp2.hm.com/hmgoepprod?set=source[/e5/ff/e5ff221890f640a53d4240a6801d2a1283c36fcf.jpg],origin[dam],category[men_tshirtstanks_multipacks],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",     
+    },
+    {
+        id: 6,
+        img: "https://lp2.hm.com/hmgoepprod?set=source[/e5/ff/e5ff221890f640a53d4240a6801d2a1283c36fcf.jpg],origin[dam],category[men_tshirtstanks_multipacks],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",     
+    },
+    {
+        id: 7,
+        img: "https://lp2.hm.com/hmgoepprod?set=source[/e5/ff/e5ff221890f640a53d4240a6801d2a1283c36fcf.jpg],origin[dam],category[men_tshirtstanks_multipacks],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",     
+    },
+    {
+        id: 8,
+        img: "https://lp2.hm.com/hmgoepprod?set=source[/e5/ff/e5ff221890f640a53d4240a6801d2a1283c36fcf.jpg],origin[dam],category[men_tshirtstanks_multipacks],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",     
+    },
 ]
