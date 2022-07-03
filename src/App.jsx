0b39511b3ac33d21f048efe674ef295430ad4dd9
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-07-03 12:04:41 +0530

    Login and register page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import Product from './pages/Product';
 import ProductList from './pages/ProductList';
+import Register from './pages/Register';
 
 const App = () => {
   return (
     <div>
     {/* <Home /> */}
-    <Product />
+    {/* <Product /> */}
+    {/* <Register /> */}
+    <Login />
     </div>
   )
 }
